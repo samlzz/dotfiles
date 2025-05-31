983ef78b91a839e1471e3e1a2fe35ff5e11f9957
um4ss <sliziard@icloud.com>
2025-05-31 21:35:33 +0200

    fix(bootstrap): call tpm script with absolute path

--- run_once_after_bootstrap.sh
+++ run_once_after_bootstrap.sh
@@ -12,5 +12,5 @@ if [[ ! -d "$TPM_DIR" ]]; then
   git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
 fi
 
-./"$TPM_DIR"/bin/install_plugins
+"$TPM_DIR"/bin/install_plugins
 echo "Please ensure all dependecies are installed and $USER is in 'input' group"
