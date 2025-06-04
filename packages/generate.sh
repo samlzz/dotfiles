fea5317f6cc13c509d718a2c69402a86fc4f3eb5
um4ss <sliziard@icloud.com>
2025-06-04 13:36:44 +0200

    fix(packages): update dependencies list

--- packages/generate.sh
+++ packages/generate.sh
@@ -4,11 +4,10 @@ PACKAGES_DIR="$(chezmoi source-path)/packages"
 
 mkdir -p "$PACKAGES_DIR"
 
-pacman -Qqe > "$PACKAGES_DIR/pacman.txt"
+pacman -Qqe >"$PACKAGES_DIR/pacman.txt"
 
-paru -Qqm > "$PACKAGES_DIR/aur.txt"
+paru -Qqm >"$PACKAGES_DIR/aur.txt"
 
-snap list | awk 'NR>1 {print $1}' > "$PACKAGES_DIR/snap.txt"
+snap list | awk 'NR>1 {print $1}' >"$PACKAGES_DIR/snap.txt"
 
 echo "[✔] Dependecies files updated."
-
