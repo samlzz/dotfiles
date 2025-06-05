dfb2c75fce37ceb90863c9425b7b87b8e8b6c58d
um4ss <sliziard@icloud.com>
2025-06-05 12:36:58 +0200

    fix(bootstrap): give them exec auth & correct vim share path

--- run_once_after_bootstrap.sh
+++ run_once_after_bootstrap.sh
@@ -155,7 +155,7 @@ main() {
 	install_tmux_plugins || return 1
 	install_usr_executables || return 1
 	install_hyprland_plugins || return 1
-	creates_named_subdirs "$HOME/.share/vim" "backup" "swap" "undo" || return 1
+	creates_named_subdirs "$HOME/.local/share/vim" "backup" "swap" "undo" || return 1
 	printf "Initial setup complete. Make sure all required dependencies are installed.\n"
 }
 
