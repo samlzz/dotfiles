245824eff5e83d6002535e6bd452a82aa992c5ea
um4ss <sliziard@icloud.com>
2025-06-11 00:26:55 +0200

    fix(executables): add some missing autocompletion

--- dot_local/share/zsh/completion/executable_custom_completion.zsh
+++ dot_local/share/zsh/completion/executable_custom_completion.zsh
@@ -40,3 +40,25 @@ _managedns() {
 	_values 'DNS State' on off state
 }
 compdef _managedns managedns
+
+# Completion for wallpaperctl
+_wallpaperctl() {
+  local -a subcmds
+  subcmds=('set:Set a specific wallpaper' 'rd:Set a random wallpaper')
+
+  local wall_dir="${WALL_DIR:-$HOME/Pictures/wallpapers}"
+
+  _arguments -C \
+    "1:command:((set\:Set\ a\ wallpaper rd\:Set\ a\ random\ one))" \
+    "2::image file:_files -W $wall_dir" \
+    && return 0
+}
+compdef _wallpaperctl wallpaperctl
+
+# Completion for powerctl
+_powertctl() {
+	_arguments -C \
+	  "1:action:(suspend hibernate poweroff reboot)" \
+	  "*:: :->args"	
+}
+compdef _powerctl powerctl
