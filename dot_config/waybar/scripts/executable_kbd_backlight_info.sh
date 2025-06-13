275928dec867bdd4971eeafc8a1efdc4d90ba561
um4ss <sliziard@icloud.com>
2025-06-13 02:49:03 +0200

    fix(waybar): correct kbd backlight script

--- dot_config/waybar/scripts/executable_kbd_backlight_info.sh
+++ dot_config/waybar/scripts/executable_kbd_backlight_info.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kbd_pct=$(brightnessctl -rd chromeos::kbd_backlight 2>/dev/null | awk '/Current brightness/ {print $4}' | tr -d '()%')
+kbd_pct=$(brightnessctl -d chromeos::kbd_backlight 2>/dev/null | awk '/Current brightness/ {print $4}' | tr -d '()%')
 
 # Affiche en JSON pour Waybar
 echo "{\"percentage\": $kbd_pct, \"icon\": \"\"}"
