ddbf763dd10eace5c8b639f979672947abe3853a
um4ss <sliziard@icloud.com>
2026-05-28 14:33:15 +0200

    refactor!: remove all miss sync chezmoi config
    This is for preparing stow config commits

--- dot_config/waybar/scripts/executable_kbd_backlight_info.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-kbd_pct=$(brightnessctl -d chromeos::kbd_backlight 2>/dev/null | awk '/Current brightness/ {print $4}' | tr -d '()%')
-
-# Affiche en JSON pour Waybar
-echo "{\"percentage\": $kbd_pct, \"icon\": \"\"}"
-
-
