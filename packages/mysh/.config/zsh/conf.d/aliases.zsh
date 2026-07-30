aa6616d203ca7c7c78b3b28f636b6bfb63d06fec
um4ss <sliziard@icloud.com>
2026-07-30 13:21:31 +0200

    feat(aliases): replace grep by ripgrep cmd

--- packages/mysh/.config/zsh/conf.d/aliases.zsh
+++ packages/mysh/.config/zsh/conf.d/aliases.zsh
@@ -1,5 +1,4 @@
 # Replace ls with exa
-
 if command -v 'eza' &> /dev/null; then
 	alias ls='eza --group-directories-first'
 	alias la='eza -a --group-directories-first'   # all files and dirs
@@ -15,6 +14,10 @@ else
 		alias lta='tree -a' # tree listing of all files and dirs
 	fi
 fi
+
+# Replace grep with rip grep
+alias grep='rg'
+
 alias cl='clear'
 
 if command -v 'valgrind' &> /dev/null; then
